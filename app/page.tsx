7518f40dbe39c6febd5ddbb0e6bd8fca094a1d5a
Kimi Lorenzo <huangaina2103.hw@gmail.com>
2026-07-07 22:52:15 +0700

    test

--- app/page.tsx
+++ app/page.tsx
@@ -1,21 +1,32 @@
-async function getData() {
-  const res = await fetch(
-    "https://responsive-ui-nextjs-964jsl1cd-kimilorenzos-projects.vercel.app/api/data",
-    {
-      cache: "no-store",
-    }
-  );
+import { Metadata } from "next";
 
-  return {
-    status: res.status,
-    contentType: res.headers.get("content-type"),
+type profileData = {
+  data: {
+    name: string;
+    description: string;
   };
+};
+
+export const metadata: Metadata = {
+  title: "Belajar NextJS",
+  description: "Website keren banget",
+};
+
+async function getData(): Promise<profileData> {
+  const res = await fetch("https://responsive-ui-nextjs-964jsl1cd-kimilorenzos-projects.vercel.app/api/data", {
+    cache: "no-store",
+  });
+
+  return res.json();
 }
 
 export default async function Home() {
   const data = await getData();
 
   return (
-    <pre>{JSON.stringify(data, null, 2)}</pre>
+    <div className={`flex flex-col items-center justify-center h-screen bg-yellow-100`}>
+      <h1 className="text-[#ff4d32] font-bold text-2xl underline decoration-[#ff4d32]">{data.data.name}</h1>
+      <p className="text-[#ff4d32] font-light">{data.data.description}</p>
+    </div>
   );
 }
