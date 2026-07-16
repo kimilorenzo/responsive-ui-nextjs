02a7e6a146feb9f8ca45afa0b676ac401837eb73
Kimi Lorenzo <huangaina2103.hw@gmail.com>
2026-07-16 21:23:23 +0700

    Finished About Page

--- app/page.tsx
+++ app/page.tsx
@@ -22,11 +22,49 @@ async function getData(): Promise<profileData> {
 
 export default async function Home() {
   const data = await getData();
+  const frontendSkills = [
+    { id: 1, name: "NextJS" },
+    { id: 2, name: "ReactJS" },
+    { id: 3, name: "Svelte" },
+  ];
+
+  const backendSkills = [
+    { id: 1, name: "NestJS" },
+    { id: 2, name: "ExpressJS" },
+    { id: 3, name: "Spring Boot Java" },
+  ];
 
   return (
-    <div className={`flex flex-col items-center justify-center h-screen bg-yellow-100`}>
-      <h1 className="text-[#ff4d32] font-bold text-2xl underline decoration-[#ff4d32]">{data.data.name}</h1>
-      <p className="text-[#ff4d32] font-light">{data.data.description}</p>
+    <div>
+      <div id="home" className={`flex flex-col items-center justify-center h-screen bg-yellow-100`}>
+        <h1 className="text-[#ff4d32] font-bold text-2xl underline decoration-[#ff4d32]">{data.data.name}</h1>
+        <p className="text-[#ff4d32] font-light">{data.data.description}</p>
+      </div>
+      <div id="about" className={`flex flex-col items-center justify-center h-screen bg-[#ff4d32] gap-4`}>
+        <div className="flex flex-col bg-[#ff4d32] gap-4">
+          <h1 className="text-2xl font-bold text-yellow-100 underline decoration-yellow-100">Skills:</h1>
+          <div>
+            <h1 className="font-bold text-yellow-100">Frontend:</h1>
+            <ul>
+              {frontendSkills.map((skill) => (
+                <li key={skill.id}>
+                  <p className="text-yellow-100">{skill.name}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <div>
+            <h1 className="text-2xl font-bold text-yellow-100 underline decoration-yellow-100">Backend:</h1>
+            <ul>
+              {backendSkills.map((skill) => (
+                <li key={skill.id}>
+                  <p className="text-yellow-100">{skill.name}</p>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
